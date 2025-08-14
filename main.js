518d83254426f0d17cbf8d51ab7e6a6455d6fba9
Fatin <abdelrahimfa@gmail.com>
2025-08-14 14:05:36 -0500

    Add 'playGame' function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* TOP Rock Paper Scissors
- * Fatin Abdelrahim
+ * Fatin A.
  * Last Update: 8/14/2025
 */
 
@@ -29,20 +29,45 @@ function playRound(humanChoice, computerChoice) {
                 (humanChoice === "scissors" && computerChoice === "paper") || 
                 (humanChoice === "paper" && computerChoice === "rock")) {
         console.log("You win!");
-        humanScore++;
-        return;
+        return "human";
     } else {
         console.log("You lose!");
-        computerScore++;
-        return;
+        return "computer";
     }
 }
 
-let humanScore, computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 1; i < 6; i++) {
+        console.log(`Round ${i}!`);
 
-console.log(humanSelection);
-console.log(computerSelection);
+        const humanSelection = getHumanChoice();
+        console.log(`You chose: ${humanSelection}`);
 
-playRound(humanSelection, computerSelection);
+        const computerSelection = getComputerChoice();
+        console.log(`COM chose: ${computerSelection}`);
+
+        let winner = playRound(humanSelection, computerSelection);
+
+        if (winner === "human") {
+            humanScore++;
+        } else if (winner === "computer") {
+            computerScore++;
+        }
+    }
+    
+    if (humanScore === computerScore) {
+        console.log(`You both win! 
+                    Results: You - ${humanScore} | COM - ${computerScore}`);
+    } else if (humanScore > computerScore) {
+        console.log(`You're the ultimate champion! 
+            Results: You - ${humanScore} | COM - ${computerScore}`);
+    } else {
+        console.log(`Better luck next time!
+            Results: You - ${humanScore} | COM - ${computerScore}`);
+    }
+}
+
+playGame();
